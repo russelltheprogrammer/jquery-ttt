a2671363a967c6a36506b6e7d44e07a0c5f9685b
Russell <russelltheprogrammer@gmail.com>
2024-03-21 17:55:02 -0400

    work on project

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
-    $('h1').click(function() {
-        $(this).css('color', 'red');
+$(document).ready( () => {
+
+    $('.hide').hide();
+
+
+    $('#header-section h1').on('mouseenter', (event) => {
+        $(event.currentTarget).css('color', 'red')
+        .on('mouseleave', () => { 
+            $(event.currentTarget).css('color', 'black')
+        });
     });
+
+    $('#secret-section').on('mouseenter', () => {
+        $('.hide').show()
+        // .on('mouseleave', () => {
+        //     $('.hide').hide();
+        // })
+    });
+    
 });
